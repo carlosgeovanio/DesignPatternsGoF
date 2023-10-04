d3aac5503138361314d2a16df7b6dda39826f848
Gil <carlosgeovaniosilva@gmail.com>
2023-10-04 20:49:17 -0300

    Included test project

---
+++ DesignPatternsUnitTests/TestFoo.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatternsGoF.Creational.Singleton;
+using Moq;
+
+namespace DesignPatternsUnit.Tests
+{
+    [TestClass]
+    public class TestFoo
+    {
+        [TestMethod]
+        public void Logger_GetInstance_ReturnsSameInstance()
+        {
+            // Arrange
+            var logger1 = Logger.GetInstance();
+            var logger2 = Logger.GetInstance();
+
+            // Assert
+            Assert.AreSame(logger1, logger2);
+        }
+    }
+}

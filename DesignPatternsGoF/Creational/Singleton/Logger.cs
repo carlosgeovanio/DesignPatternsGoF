b91d9f12c496e0c50a7d3259ffa3681ddcffd4fa
Gil <carlosgeovaniosilva@gmail.com>
2023-10-05 16:06:05 -0300

    Changed main project to Library and added folder structure for unit testing

--- DesignPatternsGoF/Creational/Singleton/Logger.cs
+++ DesignPatternsGoF/Creational/Singleton/Logger.cs
@@ -2,24 +2,29 @@
 {
     public class Logger
     {
-        private static Logger instance;
+        private static Logger? _instance;
+        private List<string> _logs;
 
-        private Logger() { }
+        private Logger() {
+            _logs = new List<string>();
+        }
 
         public static Logger GetInstance()
         {
-            if (instance == null)
-            {
-                instance = new Logger();
-                // Configurações de inicialização, se necessário
-            }
-            return instance;
+            if (_instance == null)
+                _instance = new Logger();
+
+            return _instance;
         }
 
         public void Log(string message)
         {
-            // Lógica para registrar a mensagem de log
-            Console.WriteLine($"Logging: {message}");
+            _logs.Add($"[{DateTime.Now}] {message}");
+        }
+
+        public IEnumerable<string> GetLogs()
+        {
+            return _logs;
         }
     }
 }
